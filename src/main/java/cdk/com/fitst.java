a5602bb4a09042b5fa52abcc263300e439fef8a4
admin <cdk@cdk.com>
2023-01-29 20:55:20 +0800

    three

--- src/main/java/cdk/com/fitst.java
+++ src/main/java/cdk/com/fitst.java
@@ -8,5 +8,6 @@ public class fitst {
     public static void main(String[] args) {
         System.out.println("你好 git");
         System.out.println("你好");
+        System.out.println("好");
     }
 }
