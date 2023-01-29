1b331335c1373f025ef712a673919a8ccbd9c7bb
admin <cdk@cdk.com>
2023-01-29 20:50:46 +0800

    one

---
+++ src/main/java/cdk/com/fitst.java
@@ -0,0 +1,11 @@
+package cdk.com;
+
+/**
+ * @author cdk
+ * @date 2023/1/29 - 20:44
+ */
+public class fitst {
+    public static void main(String[] args) {
+        System.out.println("你好 git");
+    }
+}
